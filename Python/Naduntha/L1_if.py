d8641093c4fc0cf94909ce0fe2e355f6bdf5c1a1
naduntha5017p <naduntha5017p@gmail.com>
2024-01-17 22:02:17 +0530

    naduntha

---
+++ Python/Naduntha/L1_if.py
@@ -0,0 +1,30 @@
+'''
+x = int(input("Enter a number"));
+
+if x>10:
+    print(x ,"is greater than 10")
+else :
+    print(x ," is less than 10")    
+'''
+#method 01 
+marks = int(input("Enter Your Marks :"));
+
+if marks > 100:
+    print ("Error")
+elif marks >= 75 :
+    print ("A")
+elif marks >= 65:
+    print("B")    
+elif marks >= 55:
+    print("C")    
+elif marks >= 35:
+    print("S") 
+elif marks < 35 < 0:
+    print ("F")
+else :
+    print("Error")
+
+
+
+
+    
