b6cb91e1f96390beffd2a862ed72ead14eb674c2
Sanuji Dilrukshi <sanuji.dilrukshi99.com>
2024-01-17 22:11:07 +0530

    Marks

---
+++ Python/Sanuji/L1_if.py
@@ -0,0 +1,19 @@
+marks = int(input("Enter your Marks: "))
+if marks<0 or marks>100:
+     print("Invalid Marks")  
+elif marks>=75:
+        print("A")
+elif marks>=65:
+        print("B")
+elif marks>=55:
+        print("C")
+else :
+    print("Fail")
+      
+ 
+            
+            
+           
+            
+
+            
