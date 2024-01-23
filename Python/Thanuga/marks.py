81eaf7fef6d725fcde540fc8f929abc15642f610
Thanugainduwara <johndoe@example.com>
2024-01-23 01:19:42 -0500

    Thanuga

---
+++ Python/Thanuga/marks.py
@@ -0,0 +1,11 @@
+x = int(input("Enter marks"))
+if x>=75:
+    print("you have grade A")
+elif x>=65:
+    print("you have grade B")
+elif x>55:
+    print("you have grade C")
+elif x>35:
+    print("you have Grade S")
+elif x:
+    print("you are faill")
